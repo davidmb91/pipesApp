a708ffd66b7c8838298db1010aed4384235dd779
David Moreno Buj <davidmb91@gmail.com>
2023-08-25 13:35:16 +0200

    Cambiado menu a menubar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 
+import { MenubarModule } from 'primeng/menubar';
 import { MenuModule } from 'primeng/menu';
 
 @NgModule({
   exports: [
+    MenubarModule,
     MenuModule
   ]
 })
