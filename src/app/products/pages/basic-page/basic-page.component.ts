a70805e0ba35d2c4f88072775be458dd0e8bd5c5
David Moreno Buj <davidmb91@gmail.com>
2023-08-26 13:57:37 +0200

    UpperCase, LowerCase y TitleCase Pipes

--- src/app/products/pages/basic-page/basic-page.component.ts
+++ src/app/products/pages/basic-page/basic-page.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  public nameLower: string = 'david';
+  public nameUpper: string = 'DAVID';
+  public fullName: string = 'daVId MoREno';
 }
