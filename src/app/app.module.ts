de13600497c9eeb8214c61f478d550394cc0033b
David Moreno Buj <davidmb91@gmail.com>
2023-09-26 18:01:59 +0200

    Carga idioma global y carga pipes con idiomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
+// Configuración locale de la app
+import localesEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common'
+
+registerLocaleData(localesEs);
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
@@ -16,7 +24,11 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
