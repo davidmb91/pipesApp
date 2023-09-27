85e66c567a5348b56a2a33ff1a6dbe2a9cfb303a
David Moreno Buj <davidmb91@gmail.com>
2023-09-27 21:17:15 +0200

    i18n pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./uncommon-page.component.css']
 })
 export class UncommonPageComponent {
+  // i18n Select
+  public name: string = 'David';
+  public gender: 'male'|'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  };
+  // i18n Plural
+  public clients: string[] = ['John', 'Jane', 'Michael', 'Emily', 'David', 'Sarah', 'Christopher', 'Jennifer', 'Matthew', 'Jessica'];
+  public clientsMap = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+  };
 
+  changeClient(){
+    this.name = 'Bea';
+    this.gender = 'female';
+  }
+
+  deleteClient(){
+    this.clients.shift();
+  }
 }
